63fed969f846218f8746eb58618c8ec13333272b
swapnilbhakare <swapnilbhakare7@gmail.com>
2023-05-22 21:36:25 +0530

    passed the expense using for loop form app component to expeseItem

--- src/App.js
+++ src/App.js
@@ -1,9 +1,40 @@
 import "./App.css";
 import ExpenseItem from "./Components/ExpenseItem";
 function App() {
+  const expenses = [
+    {
+      id: "e1",
+      title: "Car Insurance",
+      amount: 294.67,
+      date: new Date(2023, 5, 22),
+      location: "pune",
+    },
+    {
+      id: "e2",
+      title: "new Mobile",
+      amount: 500.67,
+      date: new Date(2023, 5, 22),
+      location: "pune",
+    },
+    {
+      id: "e3",
+      title: "New dresses",
+      amount: 400.67,
+      date: new Date(2023, 5, 22),
+      location: "pune",
+    },
+  ];
   return (
     <div>
-      <ExpenseItem />
+      {expenses.map((expense, index) => (
+        <ExpenseItem
+          key={expense.id}
+          title={expense.title}
+          amount={expense.amount}
+          date={expense.date}
+          location={expense.location}
+        />
+      ))}
     </div>
   );
 }
--- src/Components/ExpenseItem.js
+++ src/Components/ExpenseItem.js
@@ -1,19 +1,14 @@
 import React from "react";
 import "./ExpenseItem.css";
-function ExpenseItem() {
-  const expenseDate = new Date(2021, 2, 28);
-  const expenseTitle = "car insurance";
-  const expenseAmout = 244.55;
-  const LocationOfExpenditure = "pune";
-
+function ExpenseItem(props) {
   return (
     <>
       <div className="expense-item">
-        <div>{expenseDate.toDateString()}</div>
+        <div>{props.date?.toISOString()}</div>
         <div className="expense-item__description">
-          <h2>{expenseTitle}</h2>
-          <p>{LocationOfExpenditure}</p>
-          <div className="expense-item__price ">$ {expenseAmout}</div>
+          <h2>{props.title}</h2>
+          <p>{props.location}</p>
+          <div className="expense-item__price ">$ {props.amount}</div>
         </div>
       </div>
     </>
