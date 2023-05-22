20eb7f6ff931265102aeb2bb3416bc107263edb0
swapnilbhakare <swapnilbhakare7@gmail.com>
2023-05-22 22:10:06 +0530

    breakdown the expense item into expenseDate and expense details components and added style to date component

--- src/Components/ExpenseItem.js
+++ src/Components/ExpenseItem.js
@@ -1,15 +1,18 @@
 import React from "react";
 import "./ExpenseItem.css";
+import ExpenseDate from "./ExpenseDate";
+import ExpenseDetails from "./ExpenseDetails";
 function ExpenseItem(props) {
   return (
     <>
       <div className="expense-item">
-        <div>{props.date?.toISOString()}</div>
-        <div className="expense-item__description">
-          <h2>{props.title}</h2>
-          <p>{props.location}</p>
-          <div className="expense-item__price ">$ {props.amount}</div>
-        </div>
+        <ExpenseDate date={props.date} />
+
+        <ExpenseDetails
+          title={props.title}
+          location={props.location}
+          amount={props.amount}
+        />
       </div>
     </>
   );
