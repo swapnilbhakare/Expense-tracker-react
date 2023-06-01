2af32f0695d57affd77c6b496cfd63641a637ce1
swapnilbhakare <swapnilbhakare7@gmail.com>
2023-06-01 22:02:54 +0530

    added button and functionality to change current amout into 100$

--- src/Components/Expenses/ExpenseItem.js
+++ src/Components/Expenses/ExpenseItem.js
@@ -1,11 +1,17 @@
-import React from "react";
+import React, { useState } from "react";
 import "./ExpenseItem.css";
 import ExpenseDate from "./ExpenseDate";
 import ExpenseDetails from "./ExpenseDetails";
 import Card from "../UI/Card";
 const ExpenseItem = (props) => {
+  const [title, setTitle] = useState(props.title);
+  const [amount, setAmount] = useState(props.amount);
+
   const clickHandler = () => {
-    console.log("clicked!!");
+    setTitle("update");
+  };
+  const expenseHandler = () => {
+    setAmount("100$");
   };
   const deleteHandler = (evenet) => {};
   return (
@@ -14,13 +20,16 @@ const ExpenseItem = (props) => {
         <ExpenseDate date={props.date} />
 
         <ExpenseDetails
-          title={props.title}
+          title={title}
           location={props.location}
-          amount={props.amount}
+          amount={amount}
         />
         <button onClick={clickHandler}>Change Title</button>
         <button className="deleteButton" onClick={deleteHandler}>
           Delete Expense
+        </button>
+        <button className="deleteButton" onClick={expenseHandler}>
+          changes expense
         </button>
       </Card>
     </>
